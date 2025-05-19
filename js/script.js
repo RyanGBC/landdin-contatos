a3a0f7178b3d363d17e3207de7a3760295a22d46
RyanGBC <ryangabrj@gmail.com>
2025-05-19 17:09:50 -0300

    fix: corrigido links do LinkedIn e GitHub para Ryan

--- js/script.js
+++ js/script.js
@@ -46,8 +46,8 @@ const teamMembers = [
         bio: "sou um dos fundadores da startup e sou responsável pela parte do backend e banco de dados.",
         email: "ryangabrj@gmail.com",
         whatsapp: "#",
-        linkedin: "linkedin.com/in/ryangabrielcosta/",
-        github: "github.com/RyanGBC",
+        linkedin: "https://linkedin.com/in/ryangabrielcosta/",
+        github: "https://github.com/RyanGBC",
         photo: "img/ryan.png"
     },
     {
